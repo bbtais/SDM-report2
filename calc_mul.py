d119a5030465af4b3508597211f89c781e014f5f
BABA Taisei <t243369@xdev09.edu.tut.ac.jp>
2025-02-04 18:26:08 +0900

    modify calc_mul.py

--- calc_mul.py
+++ calc_mul.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
 import re
+#import testrunner
                 
 def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
+        if type(A)==int and type(B)==int:
+                a=int(A)
+                b=int(B)
+                if 0<a and a<1000 and 0<b and b<1000:
                         valid=True
                 else:
                         valid=False
